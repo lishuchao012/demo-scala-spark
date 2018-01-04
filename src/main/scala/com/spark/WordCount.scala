622aa2b7c4b7cbdfece4a05390fd2807de2f3046
master <master@master-ThinkPad-T460>
2018-01-04 13:37:25 +0800

    add postgres function

--- src/main/scala/com/spark/WordCount.scala
+++ src/main/scala/com/spark/WordCount.scala
@@ -2,14 +2,16 @@ package com.spark
 import org.apache.spark.SparkContext  
 import org.apache.spark.SparkContext._  
 import org.apache.spark.SparkConf  
+import org.apache.spark.sql.SQLContext
 
 object WordCount {
   
   def main(args: Array[String]) {
     println("scala start");
     //countLetter();
-    //callHdfs();
-    estimatePi();
+    //visitHdfs();
+    //estimatePi();
+    visitDB();
   }
   
   def countLetter (){
@@ -22,7 +24,7 @@ object WordCount {
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))  
   }
   
-  def callHdfs (){
+  def visitHdfs (){
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]") 
     val sc = new SparkContext(conf) 
     val textFile = sc.textFile("hdfs://localhost:9000//user/craw.sql")
@@ -40,5 +42,20 @@ object WordCount {
       x*x + y*y < 1
     }.count()
     println(s"Pi is roughly ${4.0 * count / NUM_SAMPLES}")
+  }
+  
+  def visitDB (){
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")  
+    val sc = new SparkContext(conf);  
+    val sqlContext = new SQLContext(sc);
+    val url = "jdbc:postgresql://localhost:5432/meta?user=postgres&password=123456"
+    val df = sqlContext.read.format("jdbc").option("url", url).option("dbtable", "sys_user").load()
+    
+    // Looks the schema of this DataFrame.
+    df.printSchema()
+    
+    // Counts people by age
+    val countsByAge = df.groupBy("password").count()
+    countsByAge.show()
   }
 }
